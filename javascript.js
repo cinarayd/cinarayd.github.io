ba47e13dc0d5fdd096c7572560f4ba78a2779b8b
cnarasdas <k.ejderler@gmail.com>
2024-12-11 18:27:14 +0300

    Dinamikleşitrme

--- javascript.js
+++ javascript.js
@@ -1,1 +1,6 @@
-console.log("Hello World");
+document.getElementById("pageTitle").innerText = "Hello, Welcome to My GitHub Page!";
+document.getElementById("pageDescription").innerText = "This is now a dynamic content powered by JavaScript!";
+document.getElementById("changeContentButton").addEventListener("click", function() {
+    document.getElementById("pageTitle").innerText = "Content Changed!";
+    document.getElementById("pageDescription").innerText = "The content has been changed by clicking the button.";
+});
