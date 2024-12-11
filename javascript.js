5a11141bf818c0758bd068c0e61c55015130c80a
cnarasdas <k.ejderler@gmail.com>
2024-12-11 18:35:12 +0300

    First changes: added HTML, JS, and CSS

--- javascript.js
+++ javascript.js
@@ -1,9 +1,11 @@
 window.onload = function() {
-    document.getElementById("pageTitle").innerText = "Hello, Welcome to My GitHub Page!";
-    document.getElementById("pageDescription").innerText = "This is now a dynamic content powered by JavaScript!";
-    
+    document.getElementById("postTitle").innerText = "How to Create a Dynamic Website with GitHub Pages";
+    document.getElementById("postDate").innerText = "Posted on December 11, 2024";
+    document.getElementById("postContent").innerText = "This is a simple blog post to demonstrate how to create a dynamic website using GitHub Pages.";
+
     document.getElementById("changeContentButton").addEventListener("click", function() {
-        document.getElementById("pageTitle").innerText = "Content Changed!";
-        document.getElementById("pageDescription").innerText = "The content has been changed by clicking the button.";
+        document.getElementById("postTitle").innerText = "New Content After Button Click!";
+        document.getElementById("postDate").innerText = "Updated on " + new Date().toLocaleDateString();
+        document.getElementById("postContent").innerText = "The content has been dynamically updated after clicking the button.";
     });
 };
