2973b15b23e1f108b662081fb67f8e7b54284bb7
cnarasdas <k.ejderler@gmail.com>
2024-12-11 18:30:03 +0300

    First changes: added HTML, JS, and CSS

--- javascript.js
+++ javascript.js
@@ -1,6 +1,10 @@
-document.getElementById("pageTitle").innerText = "Hello, Welcome to My GitHub Page!";
-document.getElementById("pageDescription").innerText = "This is now a dynamic content powered by JavaScript!";
-document.getElementById("changeContentButton").addEventListener("click", function() {
-    document.getElementById("pageTitle").innerText = "Content Changed!";
-    document.getElementById("pageDescription").innerText = "The content has been changed by clicking the button.";
-});
+window.onload = function() {
+    document.getElementById("pageTitle").innerText = "Hello, Welcome to My GitHub Page!";
+    document.getElementById("pageDescription").innerText = "This is now a dynamic content powered by JavaScript!";
+    
+    document.getElementById("changeContentButton").addEventListener("click", function() {
+        document.getElementById("pageTitle").innerText = "Content Changed!";
+        document.getElementById("pageDescription").innerText = "The content has been changed by clicking the button.";
+    });
+};
+
